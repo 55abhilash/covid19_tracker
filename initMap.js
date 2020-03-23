85281917e9dcbb22c13e4514e8e5d1b978e68b67
Abhilash KP <abhilashmhaisne@gmail.com>
2020-03-24 01:17:44 +0530

    Initialization of the map.

--- initMap.js
+++ initMap.js
@@ -1,5 +1,5 @@
 
-var myMap = L.map('mapid').setView([24, 16], 2);
+var myMap = L.map('mapid').setView([24, 16], 3);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiNTVhYmhpbGFzaCIsImEiOiJjazg0a2t2bW8xMnE0M25vd3FwNXQzZ3BwIn0.3dbqJ8xSkjN9lrstWlPM5g', 
 {
@@ -10,3 +10,4 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
 	zoomOffset: -1,
 	accessToken: 'pk.eyJ1IjoiNTVhYmhpbGFzaCIsImEiOiJjazg0a2t2bW8xMnE0M25vd3FwNXQzZ3BwIn0.3dbqJ8xSkjN9lrstWlPM5g'
 }).addTo(myMap);
+
